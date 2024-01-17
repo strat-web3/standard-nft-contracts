eeb9b3f3ad46084af614c36008e600282b8b667f
julienbrg <julien@strat.cc>
2024-01-17 09:39:45 +0100

    add mint

--- tasks/mint.ts
+++ tasks/mint.ts
@@ -1,32 +1,34 @@
 import { task } from "hardhat/config"
 var msg = require("cli-color").xterm(39).bgXterm(128)
-import * as artheraTestnetDeploymentData from "../deployments/arthera-testnet/Basic.json"
-import * as sepoliaDeploymentData from "../deployments/sepolia/Basic.json"
-import * as artheraDeploymentData from "../deployments/arthera/Basic.json"
-import * as opSepoliaDeploymentData from "../deployments/op-sepolia/Basic.json"
+// import * as artheraTestnetDeploymentData from "../deployments/arthera-testnet/NFT.json"
+import * as sepoliaDeploymentData from "../deployments/sepolia/NFT.json"
+// import * as artheraDeploymentData from "../deployments/arthera/NFT.json"
+// import * as opSepoliaDeploymentData from "../deployments/op-sepolia/NFT.json"
 
-task("mint", "Mint a given amount of ERC-20 tokens")
-    .addParam("amount")
-    .setAction(async (amount, hre) => {
+task("mint", "Mint a given amount of ERC-20 tokens").setAction(
+    async (amount, hre) => {
         const [signer] = await ethers.getSigners()
         const NFT = await ethers.getContractFactory("NFT")
         let addr
         switch (hre.network.name) {
-            case "arthera":
-                addr = artheraDeploymentData.address
-                break
-            case "arthera-testnet":
-                addr = artheraTestnetDeploymentData.address
-                break
+            // case "arthera":
+            //     addr = artheraDeploymentData.address
+            //     break
+            // case "arthera-testnet":
+            //     addr = artheraTestnetDeploymentData.address
+            //     break
             case "sepolia":
                 addr = sepoliaDeploymentData.address
                 break
-            case "op-sepolia":
-                addr = opSepoliaDeploymentData.address
-                break
+            // case "op-sepolia":
+            //     addr = opSepoliaDeploymentData.address
+            //     break
         }
         const nft = new ethers.Contract(addr, NFT.interface, signer)
-        const mint = await nft.safeMint(await ethers.parseEther(amount.amount))
+        const uri =
+            "https://bafkreidrrwa6eckvudnokxsttfayckjvilqpote6xn3fc5beler76py57u.ipfs.w3s.link/"
+        const recipient = "0x27292E1a901E3E0bE7d144aDba4CAD07da2d8a42"
+        const mint = await nft.safeMint(recipient, uri)
         const hash = mint.hash
         console.log(
             "Minted",
@@ -34,4 +36,5 @@ task("mint", "Mint a given amount of ERC-20 tokens")
             "units. \n\nTx hash:",
             msg(hash)
         )
-    })
+    }
+)
